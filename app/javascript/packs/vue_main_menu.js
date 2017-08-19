985518f728ee2454babf0812b0c4e0ecbafddf31
Frank Vinci <fv42wid@yahoo.com>
2017-08-19 12:38:30 -0400

    toolbar navigation works

--- app/javascript/packs/vue_main_menu.js
+++ app/javascript/packs/vue_main_menu.js
@@ -12,10 +12,26 @@ document.addEventListener('turbolinks:load', () => {
     var vueMainMenu = new Vue({
         el: element,
         data: {
-            sideNav: false
+            sideNav: false,
+            menuItems: [
+                {icon: 'supervisor_account', title: 'Employees', link: '/employees'},
+                {icon: 'room', title: 'Organize Meetup', link: '#'},
+                {icon: 'person', title: 'Profile', link: '#'},
+                {icon: 'face', title: 'Sign Up', link: '#'},
+                {icon: 'lock_open', title: 'Sign In', link: '#'}
+            ]
         },
         created: function () {
             console.log('test')
+        },
+        methods: {
+            goHome: function() {
+               Turbolinks.visit('/')
+            },
+            navigateTo: function(link) {
+                console.log(link)
+                Turbolinks.visit(link)
+            }
         }
     })
 })
